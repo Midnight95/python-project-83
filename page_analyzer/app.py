5f4a5be3dbe5a1355468a122b636714feb832669
midnight <ivanator666@gmail.com>
2023-09-02 01:19:08 +0300

    created db class

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,28 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
+import psycopg2
 load_dotenv()
+
+# Database block
+
+
+class Database:
+    def __int__(self, url):
+        self.url = url
+        self.conn = psycopg2.connect(url)
+        self.cursor = self.conn.cursor()
+
+    def insert(self):
+        self.cursor.execute('INSERT INTO urls (name) VALUES (%s)', self.url)
+        self.conn.commit()
+
+    def close(self):
+        self.conn.close()
+
+# Validator block
+
+# App block
+
 
 app = Flask(__name__)
 
@@ -10,3 +32,6 @@ def index():
     return render_template('index.html')
 
 
+@app.get('/urls')
+def urls():
+    return render_template('urls.html')
