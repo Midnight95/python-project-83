1b5b86b868addf6e7c6a6a46dbee40791f0c23c7
midnight <ivanator666@gmail.com>
2023-09-21 02:34:51 +0300

    made a flash message

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -2,16 +2,22 @@ from flask import (
     Flask,
     render_template,
     url_for,
-    request
+    redirect,
+    request,
+    flash
     )
+
 from dotenv import load_dotenv
 from urllib.parse import urlparse
 from validators.url import url
+
+import os
+
 load_dotenv()
 
 
 # Validator
-def validate(addr: str) -> str:
+def validate(addr: str):
     if not url:
         return 'URL обязателен'
     if not url(addr):
@@ -20,13 +26,14 @@ def validate(addr: str) -> str:
         return 'URL превышает 255 символов'
 
 
-def normalize(addr: str) -> str:
+def normalize(addr: str):
     normalized_addr = urlparse(addr)
     return f'{normalized_addr.scheme}://{normalized_addr.netloc}'
 
 
 # App block
 app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
 
 
 @app.route('/')
@@ -34,16 +41,18 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/urls', methods=['GET', 'POST'])
+@app.route('/urls/', methods=['GET', 'POST'])
 def urls():
     if request.method == 'GET':
-        pass
+        return 'Woaw!'
 
     if request.method == 'POST':
-        data = request.form.get('url')
-        errors = validate(data)
+        error = validate(request.form.get('url'))
+        if error:
+            flash(error)
+            return render_template('index.html', error=error)
 
-        return data
+        return redirect(url_for('url_info', id=1), code=302)
 
 
 @app.get('/urls/<id>')
