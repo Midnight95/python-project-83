6d2906632027c0d2858e954aabbb5bf4002f2c00
midnight <ivanator666@gmail.com>
2023-09-26 03:33:23 +0300

    updated Database class

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -3,25 +3,34 @@ import psycopg2
 
 
 class Database:
-    def __init__(self, addr):
-        self.addr = addr
+    def __init__(self, db_url: str):
+        self.db_url = db_url
 
     def __enter__(self):
-        self.conn = psycopg2.connect(self.addr)
+        self.conn = psycopg2.connect(self.db_url)
         self.cursor = self.conn.cursor()
         return self
 
-    def insert(self, address):
+    def insert(self, address: str):
         self.cursor.execute(
             "INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id;",
             (address, date.today())
         )
-        return self.cursor.fetchone()
+        entry_id = self.cursor.fetchone()
+        return entry_id[0] if entry_id else None
 
-    def check(self, address):
+    def check(self, address: str):
         self.cursor.execute(
             "SELECT id FROM urls WHERE name = (%s);",
             (address,)
+        )
+        entry_id = self.cursor.fetchone()
+        return entry_id[0] if entry_id else None
+
+    def find(self, id: int):
+        self.cursor.execute(
+            "SELECT * FROM urls WHERE id = (%s);",
+            (id,)
         )
         return self.cursor.fetchone()
 
