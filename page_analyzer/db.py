98359801cdecd0076119b6bd3d5bf0f2c334271a
midnight <ivanator666@gmail.com>
2023-09-28 00:50:54 +0300

    made a polymorphic database handling code

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,5 +1,5 @@
-from datetime import date
 import psycopg2
+import psycopg2.extras
 
 
 class Database:
@@ -8,48 +8,31 @@ class Database:
 
     def __enter__(self):
         self.conn = psycopg2.connect(self.db_url)
-        self.cursor = self.conn.cursor()
+        self.cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
         return self
 
-    def insert_urls(self, address: str):
+    def insert(self, table: str, cols: list, *args):
         self.cursor.execute(
-            "INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id;",
-            (address, date.today())
-        )
-        entry_id = self.cursor.fetchone()
-        return entry_id[0] if entry_id else None
-
-    def insert_checks(self, url_id):
-        self.cursor.execute(
-            "INSERT INTO urls_checks (url_id, created_at) VALUES (%s, %s);",
-            (url_id, date.today())
-        )
-        self.cursor.execute(
-            "SELECT * FROM urls_checks WHERE url_id = %s",
-            (url_id,)
-        )
-        return self.cursor.fetchall()
-
-    def check(self, address: str):
-        self.cursor.execute(
-            "SELECT id FROM urls WHERE name = %s;",
-            (address,)
-        )
-        entry_id = self.cursor.fetchone()
-        return entry_id[0] if entry_id else None
-
-    def find(self, id: int):
-        self.cursor.execute(
-            "SELECT * FROM urls WHERE id = (%s);",
-            (id,)
+            f'INSERT INTO {table} ({", ".join(str(i) for i in cols)}) '
+            f'VALUES ({", ".join("%s" for i in cols)}) RETURNING *;',
+            args
         )
         return self.cursor.fetchone()
 
-    def render(self):
-        self.cursor.execute("SELECT * FROM urls")
-        return self.cursor.fetchall()
+    def render(self, table, to_find=None, col=None, show='*'):
+        if to_find and col:
+            self.cursor.execute(
+                f'SELECT {show} FROM {table} WHERE {col} = (%s)',
+                (to_find,)
+                )
+            return self.cursor.fetchone()
+        else:
+            self.cursor.execute(f'SELECT {show} FROM {table}')
+            return self.cursor.fetchall()
 
     def __exit__(self, exc_type, exc_value, traceback):
+        if self.cursor is not None:
+            self.cursor.close()
         if self.conn is not None:
             if exc_type is None:
                 self.conn.commit()
