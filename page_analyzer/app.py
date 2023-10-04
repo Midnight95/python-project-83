868bdc1bf1ca0a95ceed895aa13a2d6a94ce6706
midnight <ivanator666@gmail.com>
2023-10-04 22:32:38 +0300

    added 404 and 500 codes handling

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -135,3 +135,13 @@ def check_url(id: int):
 
     flash('Страница успешно проверена', 'success')
     return redirect(url_for('url_info', id=id))
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('errors/500.html'), 500
