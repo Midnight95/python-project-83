90889fe273437d42bee828ee21127f34ea7cf762
midnight <ivanator666@gmail.com>
2023-10-13 01:34:01 +0300

    thorough db refactoring

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,65 +1,69 @@
 import psycopg2
-import psycopg2.extras
+from psycopg2.extras import DictCursor
+from datetime import date
+from flask import current_app
 
 
-class Database:
-    def __init__(self, db_url):
-        self.db_url = db_url
+def connect():
+    db_url = current_app.config.get('DATABASE_URL')
+    if not db_url:
+        raise ValueError('$DATABASE_URL is not found')
+    try:
+        conn = psycopg2.connect(db_url)
+        return conn
+    except psycopg2.Error:
+        print('Can\'t connect to the database!')
 
-    def __enter__(self):
-        self.conn = psycopg2.connect(self.db_url)
-        self.cursor = self.conn.cursor(
-            cursor_factory=psycopg2.extras.DictCursor
-        )
-        return self
 
-    def insert(self, table: str, cols, data):
-        self.cursor.execute(
-            f'INSERT INTO {table} ({", ".join(str(i) for i in cols)}) '
-            f'VALUES ({", ".join("%s" for _ in cols)}) RETURNING id;',
-            data if isinstance(data, tuple) else tuple(data)
-        )
-        return self.cursor.fetchone().get('id')
+def execute(query: str, data=None, fetch: str = None) -> int or list:
+    with connect() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(query, data, cursor_factory=DictCursor)
+            response = None
+            match fetch:
+                case 'all':
+                    response = cursor.fetchall()
+                case 'one':
+                    response = cursor.fatchone()
+            return response
 
-    def get_urls_checks(self):
-        self.cursor.execute("SELECT * FROM urls_checks")
-        data = self.cursor.fetchall()
-        return data if any(isinstance(i, list) for i in data) else [data]
 
-    def get_urls_checks_by_id(self, url_id):
-        self.cursor.execute(
-            "SELECT * FROM urls_checks WHERE url_id = (%s)",
-            (url_id,)
-        )
-        data = self.cursor.fetchall()
-        return data if any(isinstance(i, list) for i in data) else [data]
+def get_urls():
+    query = "SELECT * FROM urls"
+    response = execute(query, fetch='all')
+    return response
 
-    def get_urls(self):
-        self.cursor.execute(
-            "SELECT * FROM urls"
-        )
-        return self.cursor.fetchall()
 
-    def get_urls_id_by_name(self, name):
-        self.cursor.execute(
-            "SELECT id FROM urls WHERE name = (%s)",
-            (name,)
-        )
-        return self.cursor.fetchone().get('id')
+def get_urls_checks():
+    query = "SELECT * FROM urls_checks"
+    response = execute(query, fetch='all')
+    return response
 
-    def get_urls_by_id(self, id):
-        self.cursor.execute(
-            "SELECT * FROM urls WHERE id = (%s)",
-            (id,)
-        )
-        return self.cursor.fetchone()
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        if self.cursor is not None:
-            self.cursor.close()
-        if self.conn is not None:
-            if exc_type is None:
-                self.conn.commit()
-            else:
-                self.conn.rollback()
-            self.conn.close()
+def get_url(urls_id: int):
+    query = "SELECT * FROM urls WHERE id = (%s)"
+    response = execute(query, (urls_id,), fetch='one')
+    return response
+
+
+def find_existing_url(url):
+    query = "SELECT id FROM urls WHERE name = (%s)"
+    response = execute(query, (url,), fetch='one')
+    if response:
+        response = response.get('id')
+    return response
+
+
+def insert_urls(url):
+    query = "INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id"
+    data = url, date.today()
+    response = execute(query, data, fetch='one')
+    return response
+
+
+def insert_urls_checks(check: dict):
+    query = """
+    INSERT INTO url_checks (url_id, status_code, h1, title, description, created_at) 
+    VALUES (%(url_id)s, %(status_code)s, %(h1)s, %(title)s, %(description)s, %(created_at)s) 
+    """
+    execute(query, check)
